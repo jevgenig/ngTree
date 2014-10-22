1edc53c2fa57e9c56de06cb90e7c21786e9b8739
Jevgeni Geimanen <jevgeni.geimanen@gmail.com>
2014-10-23 01:31:40 +0300

    - CoffeeScript, Knockout and Backbone based solution - Extended testing

---
+++ koTree/js/app.coffee
@@ -0,0 +1,54 @@
+class TreeNode extends Backbone.Model
+  constructor: -> 
+    super;
+    @._binds = {}
+    @.children = ko.observableArray()
+    @.filter = ko.observable()
+    @.isOpen = ko.observable true
+    @.on 'change', (d)-> 
+      if @ is d
+        for prop,value of d.changed
+          @._binds[prop] and @._binds[prop](value)
+  load:(o)->
+    o = o or {}
+    @.set 'name',o.name
+    @.children (@._regChild new TreeNode().load value for index,value of o.children)
+    @
+  bind:(name,value) ->
+    self=@
+    bind = @._binds[name] or @._binds[name] = (ko.observable(@.get name).subscribe (v)-> self.set name,v).target;
+    @.set name,value if value isnt undefined
+    bind;
+  _regChild:(c)->
+    c.on 'change',@.trigger.bind @,'change'
+    c
+  toggle:->
+    @.isOpen !@.isOpen()
+  edit: (newName)  ->
+    (newName = newName or prompt 'Change name',@.get 'name')  and @.set 'name', newName
+  add: (newName)->
+    (newName = newName or prompt 'Enter name') and @.children.push (@._regChild new TreeNode {name:newName})
+    @.trigger 'change'
+  toJSON: ->
+    json = super() 
+    c = @.children()
+    json.children = (value.toJSON() for index,value of c) if c and c.length
+    json;
+  childrenFiltered: ->
+    filter = @.filter()
+    children = @.children()
+    if filter then _.filter children,(child)->
+      ((child.bind 'name')().indexOf filter) > -1 
+    else children
+      
+      
+    
+class App
+  constructor: ->
+    @.treeNode = treeNode = (new TreeNode {name:'Root',level:0})
+      .load JSON.parse localStorage.getItem 'koTree'
+      .on 'change', -> localStorage.setItem 'koTree',JSON.stringify treeNode.toJSON()
+  render:(el) -> 
+    ko.applyBindings @.treeNode,el.get and el.get(0) || el
+    return @
+
---
+++ test/kotest.js
@@ -0,0 +1,92 @@
+describe("Testing Model ", function () {
+    var model, newValue = 'newvalue';
+    beforeEach(function () {
+        model = new TreeNode({name: 'Root'});
+    });
+
+    it("binding persistance", function () {
+        expect(model.bind('name')).toBe(model.bind('name'));
+    });
+
+    it("set|get", function () {
+        model.set('name', newValue);
+        expect(model.get('name')).toBe(newValue);
+    });
+    it(" ==> binding", function () {
+        var b = model.bind('name');
+        model.set('name', newValue);
+        expect(b()).toBe(newValue);
+    });
+
+    it(" binding(value) ==> ", function () {
+        model.bind('name', newValue);
+        expect(model.get('name')).toBe(newValue);
+    });
+
+    it(" binding ==>", function () {
+        model.bind('name')(newValue);
+        expect(model.get('name')).toBe(newValue);
+    });
+
+    it(" load", function () {
+        model.load({name: newValue});
+        expect(model.get('name')).toBe(newValue);
+    });
+    it(" bind load", function () {
+        var b = model.bind('name');
+        model.load({name: newValue});
+        expect(b()).toBe(newValue);
+    });
+});
+
+describe("Testing children", function () {
+    var model;
+    beforeEach(function () {
+        model = new TreeNode({name: 'Root'}).load({children: [{name: 'child1'}, {name: 'child2'}, {name: 'child3'}]});
+    });
+    it(" load", function () {
+        expect(model.children().length).toBe(3);
+    });
+    it(" add", function () {
+        model.add('child4');
+        expect(model.children().length).toBe(4);
+    });
+
+    it(" add edit filter", function () {
+        var newModel = _.last(model.add('child4').children());
+        newModel.set('name', 'child1');
+        model.filter('child1');
+        expect(model.childrenFiltered().length).toBe(2);
+    });
+
+    it(" filter fresh add", function () {
+        model.add('child4').filter('child4');
+        expect(model.childrenFiltered().length).toBe(1);
+        expect(model.childrenFiltered()[0].get('name')).toBe('child4');
+
+    });
+});
+describe("Testing filtering ", function () {
+    var model, newValue = 'newvalue';
+    beforeEach(function () {
+        model = new TreeNode({name: 'Root'}).load({children: [{name: 'child1'}, {name: 'child2'}, {name: 'child3'}]});
+    });
+
+    it(" all results", function () {
+        expect(model.childrenFiltered().length).toBe(model.children().length);
+    });
+    it(" one result", function () {
+        model.filter('child1');
+        expect(model.childrenFiltered().length).toBe(1);
+    });
+    it(" as you type", function () {
+        model.filter('c');
+        expect(model.childrenFiltered().length).toBe(3);
+        model.filter('ch');
+        expect(model.childrenFiltered().length).toBe(3);
+        model.filter('child');
+        expect(model.childrenFiltered().length).toBe(3);
+        model.filter('child1');
+        expect(model.childrenFiltered().length).toBe(1);
+    });
+});
